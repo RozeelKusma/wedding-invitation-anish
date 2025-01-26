944a7d33d980002a921fee12708393424ea4f619
Rojil Kusma <Rojil@SG-LAP-20.sgdesign.com>
2025-01-26 17:42:54 +0545

    changes to background

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,16 +1,26 @@
 import Image from "next/image";
 import React from "react";
 import ganesh from "@/app/components/ganesh.png";
-import hearts from "@/app/components/hearts.png";
+import img4 from "@/app/img4.jpeg";
 
 const Hero = () => {
   return (
-    <div className="text-[#B70B9D] bg-[#f1cfe9] max-w-screen flex flex-col items-center font-allura">
-      <Image src={ganesh} alt="ganesh" height={200} />
-      <div className={`flex flex-col items-center text-5xl md:text-8xl`}>
-        <div className="">Our Wedding</div>
-        <Image src={hearts} alt="hearts" className="max-h-[500px] w-auto" />
-        <div className="pb-4">2081/10/04</div>
+    <div className="text-[#9b2289]  h-screen max-w-screen flex flex-col items-center font-allura bg-[url(./img1.jpg)] bg-center bg-cover bg-no-repeat relative font-bold">
+      <div className="bg-[#f1cfe971] absolute  h-screen w-screen flex flex-col items-center font-allura ">
+        <Image src={ganesh} alt="ganesh" height={200} />
+        <div className={`flex flex-col items-center text-5xl md:text-8xl`}>
+          <div className=" ">Our Wedding</div>
+          <div className="flex gap-4 flex-wrap">
+            {/* <Image src={img1} alt="hearts" className=" w-auto rounded-md" /> */}
+            <Image
+              src={img4}
+              alt="hearts"
+              className="max-h-[500px] w-auto rounded-md"
+            />
+          </div>
+
+          <div className=" pb-4 ">2081/10/25</div>
+        </div>
       </div>
     </div>
   );
